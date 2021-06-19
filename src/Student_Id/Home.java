4f511359347e62a0ed152ced5b63c4f23685659a
vinugawade <vinulike11@gmail.com>
2021-06-19 12:19:10 +0530

    Improvement in update Data

--- src/Student_Id/Home.java
+++ src/Student_Id/Home.java
@@ -183,7 +183,6 @@ public static String User_Loggedin;
         }
         //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             new Home("").setVisible(true);
         });
